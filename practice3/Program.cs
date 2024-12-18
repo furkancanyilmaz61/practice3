5317812a7c7b84226e3a11069db693d3fd45fd89
furkancanyilmaz61 <furkancaanyilmaz@gmail.com>
2024-12-18 19:11:15 +0300

    Add project files.

---
+++ practice3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Lütfen bir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+//Deger 10 dan buyuk mu?, kucuk mu? yoksa esit mi? sorgusu. 
+if (sayi < 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+else if (sayi > 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+
+//Tek mi? cift mi? kontrol yapisi.
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Cift sayidir.");
+}
+else
+{
+    Console.WriteLine("Tek sayidir.");
+}
